612a26f11ea192f2225e9fd4906b7d4e1a70ebf0
vijay <vijaysharma72745@gmail.com>
2025-12-16 12:12:21 +0530

    environment file add

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,46 +1,45 @@
+import { environment } from "../../../environments/environment"
 export const API_LIST = [
-
-    {
-        name: 'GET_CATEGORIES',
-        url: 'https://dummyjson.com/products/categories',
-        status: true,
-        method: 'GET'
-    },
-    {
-        name: 'GET_PRODUCTS_BY_CATEGORY',
-        url: 'https://dummyjson.com/products/category',
-        method: 'GET',
-        status: true
-    },
-    {
-        name: 'GET_PRODUCTS',
-        url: 'https://dummyjson.com/products',
-        method: 'GET',
-        status: true
-    },
-    {
-        name: 'CREATE_USER',
-        url: 'https://dummyjson.com/users/add',
-        status: true,
-        method: 'POST'
-    },
-    {
-        name: 'ADD_TO_CART',
-        url: 'https://dummyjson.com/carts/add',
-        status: true,
-        method: 'POST'
-    },
-    {
-        name: 'GET_SINGLE_PRODUCT_DETAIL',
-        url: 'https://dummyjson.com/products',
-        status: true,
-        method: 'GET'
-    },
-    {
-        name: 'GET_CARTS_LIST',
-        url: 'https://dummyjson.com/carts/user/1',
-        status: true,
-        method: 'GET'
-    }
-
-]
+  {
+    name: 'GET_CATEGORIES',
+    url: `${environment.apiBaseUrl}/products/categories`,
+    method: 'GET',
+    status: true
+  },
+  {
+    name: 'GET_PRODUCTS_BY_CATEGORY',
+    url: `${environment.apiBaseUrl}/products/category`,
+    method: 'GET',
+    status: true
+  },
+  {
+    name: 'GET_PRODUCTS',
+    url: `${environment.apiBaseUrl}/products`,
+    method: 'GET',
+    status: true
+  },
+  {
+    name: 'CREATE_USER',
+    url: `${environment.apiBaseUrl}/users/add`,
+    method: 'POST',
+    status: true
+  },
+  {
+    name: 'ADD_TO_CART',
+    url: `${environment.apiBaseUrl}/carts/add`,
+    method: 'POST',
+    status: true
+  },
+  {
+    name: 'GET_SINGLE_PRODUCT_DETAIL',
+    url: `${environment.apiBaseUrl}/products`,
+    method: 'GET',
+    status: true
+  },
+  {
+    name: 'GET_CARTS_LIST',
+    url: `${environment.apiBaseUrl}/carts/user`,
+    method: 'GET',
+    status: true
+  }
+];
